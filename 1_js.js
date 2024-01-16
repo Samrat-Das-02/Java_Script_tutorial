30048826b489913bdeb1bff41a08d069f0820b27
Samrat Das <122716699+Samrat-Das-02@users.noreply.github.com>
2024-01-16 23:05:35 +0530

    Add files via upload

---
+++ 1_js.js
@@ -0,0 +1,28 @@
+console.log(": This the first code :");
+console.log(": About the variables :");
+var a = 10;
+console.log(a);
+{
+  var a="hello";
+  console.log(a);
+}
+console.log("a = ",a);
+
+console.log(": About the let :");
+let b = 10;
+console.log(b);
+{
+  let b="hello";
+  console.log(b);
+}
+console.log("b = ",b);
+
+console.log(": About the const :");
+
+const c = 10;
+console.log(c);
+{
+  const c="hello";
+  console.log(c);
+}
+console.log("a = ",c);
